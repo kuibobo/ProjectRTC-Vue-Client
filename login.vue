edafd5ed1394e5576ba0415296a813c01a19cbf9
kuibobo <kuibobo@gmail.com>
2022-10-12 00:17:27 +0800

    init

---
+++ login.vue
@@ -0,0 +1,242 @@
+<template>
+  <div class="webrtc">
+    <div class="remoteStreams ng-scope" ng-controller="RemoteStreamsController as rtc">
+      <h2>Remote Streams</h2>
+
+      <div id="remoteVideosContainer"></div>
+
+      <table>
+        <thead>
+        <tr>
+          <th>Stream</th><th>1-way</th><th>2-way</th>
+        </tr>
+        </thead>
+        <tbody>
+        <tr v-for="item in streams">
+          <td><a>{{item.name}}</a></td>
+          <td><el-button @click="handleCall(item)">呼叫</el-button></td>
+        </tr>
+        </tbody>
+      </table>
+
+      <li style="width: 240px">
+        <el-button @click="handleLoadData">刷新</el-button>
+      </li>
+      <div class="remoteStreams">
+
+        <p class="footer">pierre@chabardes.net</p>
+
+      </div>
+    </div>
+
+    <div class="localStream ng-scope" ng-controller="LocalStreamController as localStream">
+      <p>Username: <input ng-model="localStream.name" class="ng-pristine ng-untouched ng-valid"></p>
+      <video id="localVideo" muted="muted" autoplay="true"></video>
+      <li>
+        <el-button @click="handleShowCam" v-if="!isShowVideo">开始</el-button>
+        <el-button @click="handleCloseCam" v-else>关闭</el-button>
+      </li>
+      <div ng-show="localStream.cameraIsOn" class="ng-hide">
+        <p>Share this link:</p>
+        <a class="ng-binding"></a>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import { PeerManager, Peer, requestUserMedia, attachMediaStream } from '@/utils/webrtc-client'
+
+export default {
+  name: "Login",
+  data() {
+    return {
+      camera: {},
+      streams: [],
+      isShowVideo: false,
+      client : undefined,
+    };
+  },
+  watch: {
+  },
+  mounted() {
+    console.info("created")
+    let me = this
+    me.camera.preview = window.document.getElementById('localVideo');
+    me.client = new PeerManager();
+
+    var mediaConfig = {
+      audio:true,
+      video: {
+        mandatory: {},
+        optional: []
+      }
+    };
+
+    me.$root.eventHub.$on('cameraIsOn', (data)=>{
+      console.info('cameraIsOn', data)
+      me.isShowVideo = data
+    })
+
+    me.camera.start = function(){
+      return requestUserMedia(mediaConfig)
+        .then(function(stream){
+          attachMediaStream(me.camera.preview, stream);
+          me.client.setLocalStream(stream);
+          me.camera.stream = stream;
+          me.$root.eventHub.$emit('cameraIsOn', true)
+        })
+        .catch(Error('Failed to get access to local media.'));
+    };
+    me.camera.stop = function(){
+      return new Promise(function(resolve, reject){
+        try {
+          //camera.stream.stop() no longer works
+          for( var track in me.camera.stream.getTracks() ){
+            track.stop();
+          }
+          me.camera.preview.src = '';
+          resolve();
+        } catch(error) {
+          reject(error);
+        }
+      })
+        .then(function(result){
+          me.$root.eventHub.$emit('cameraIsOn', false)
+        });
+    };
+  },
+  methods: {
+    getStreamById (id) {
+      let me = this
+      for (var i = 0; i < me.streams.length; i++) {
+        if (me.streams[i].id === id) {
+          return me.streams[i];
+        }
+      }
+    },
+
+    handleLoadData() {
+      const axios = require('axios').default;
+
+      let me = this
+// Make a request for a user with a given ID
+      axios.get('http://192.168.2.220:3002/clients')
+        .then(function (resp) {
+          // handle success
+          for(var i=0; i<resp.data.length;i++) {
+            var stream = me.getStreamById(resp.data[i].id);
+            resp.data[i].isPlaying = (!!stream) ? stream.isPLaying : false;
+          }
+          me.streams = resp.data
+          console.log(resp.data);
+        })
+    },
+
+    handleShowCam() {
+      let me = this
+      me.camera.start().then(function(result) {
+        me.client.send('readyToStream', { name: 'ChromeX' });
+      })
+        .catch(function(err) {
+          console.log(err);
+        });
+    },
+    handleCloseCam() {
+      let me = this
+      me.camera.stop()
+        .then(function(result){
+          me.client.send('leave');
+          me.client.setLocalStream(null);
+        })
+        .catch(function(err) {
+          console.log(err);
+        });
+    },
+    handleCall(stream) {
+      let me = this
+      me.camera.start()
+        .then(function(result) {
+          me.client.toggleLocalStream(stream.id);
+          if(stream.isPlaying){
+            me.client.peerRenegociate(stream.id);
+          } else {
+            me.client.peerInit(stream.id);
+          }
+          stream.isPlaying = stream.isPlaying == undefined ? true : !stream.isPlaying;
+        })
+        .catch(function(err) {
+          console.log(err);
+        });
+    },
+  }
+};
+</script>
+
+<style rel="stylesheet/scss" lang="scss">
+body {
+  padding: 30px;
+  font: 14px "Lucida Grande", Helvetica, Arial, sans-serif;
+}
+
+a {
+  color: #00b7ff;
+  text-decoration: none;
+}
+
+table, tr, td, th {margin: 0;border: 0;padding: 5px;border-collapse: collapse;}
+
+li {
+  list-style-type: none;
+  border: 2px solid black;
+  border-radius: 5px;
+  text-align: center;
+  box-shadow: 3px 3px 3px #888;
+  padding: 5px 5px 5px 5px;
+}
+
+li:hover {
+  cursor: pointer;
+  background-color: #aaa;
+}
+
+.localStream {
+  display: inline-block;
+  top: 15px;
+  right: 15px;
+  float: right;
+}
+
+.localStream video {
+  border: 5px solid rgb(119, 119, 119);
+  outline: 0px none;
+  height: 240px;
+  width: 320px;
+}
+
+.remoteStreams{
+  display: inline-block;
+  top: 15px;
+  right: 15px;
+  float: left;
+  text-align: center;
+}
+
+.remoteStreams td:hover {
+  cursor: pointer;
+  background-color: #aaa;
+}
+
+.remoteStreams .selected {
+  background-color: #444 !important;
+  text-shadow:none;
+  border-right-color: #aaa;
+  border-left: none;
+  border-radius: 3px 3px 3px 3px;
+  box-shadow:inset 1px 2px 6px #070707;
+}
+
+.footer {
+  clear: both;
+}
+</style>
---
+++ webrtc-client.js
@@ -0,0 +1,432 @@
+//import { io } from "socket.io-client";
+
+const io = require('socket.io-client');
+var PeerManager = (function () {
+
+  var localId,
+    config = {
+      peerConnectionConfig: {
+        iceServers: [
+          {"url": "stun:23.21.150.121"},
+          {"url": "stun:stun.l.google.com:19302"}
+        ]
+      },
+      peerConnectionConstraints: {
+        optional: [
+          {"DtlsSrtpKeyAgreement": true}
+        ]
+      }
+    },
+    peerDatabase = {},
+    localStream,
+    remoteVideoContainer = document.getElementById('remoteVideosContainer'),
+    socket = io('http://192.168.2.220:3001', {
+      type: Object,  // NOTE: use these options: https://socket.io/docs/v4/client-options/
+      default() {
+        return { rejectUnauthorized: false, transports: ['polling', 'websocket'] };
+      }
+    });
+
+  socket.on('message', handleMessage);
+  socket.on('id', function(id) {
+    localId = id;
+  });
+
+  function addPeer(remoteId) {
+    var peer = new Peer(config.peerConnectionConfig, config.peerConnectionConstraints);
+    peer.pc.onicecandidate = function(event) {
+      if (event.candidate) {
+        send('candidate', remoteId, {
+          label: event.candidate.sdpMLineIndex,
+          id: event.candidate.sdpMid,
+          candidate: event.candidate.candidate
+        });
+      }
+    };
+    peer.pc.onaddstream = function(event) {
+      attachMediaStream(peer.remoteVideoEl, event.stream);
+      remoteVideosContainer.appendChild(peer.remoteVideoEl);
+    };
+    peer.pc.onremovestream = function(event) {
+      peer.remoteVideoEl.src = '';
+      remoteVideosContainer.removeChild(peer.remoteVideoEl);
+    };
+    peer.pc.oniceconnectionstatechange = function(event) {
+      switch(
+        (  event.srcElement // Chrome
+          || event.target   ) // Firefox
+          .iceConnectionState) {
+        case 'disconnected':
+          remoteVideosContainer.removeChild(peer.remoteVideoEl);
+          break;
+      }
+    };
+    peerDatabase[remoteId] = peer;
+
+    return peer;
+  }
+  function answer(remoteId) {
+    var pc = peerDatabase[remoteId].pc;
+    pc.createAnswer(
+      function(sessionDescription) {
+        pc.setLocalDescription(sessionDescription);
+        send('answer', remoteId, sessionDescription);
+      },
+      error
+    );
+  }
+  function offer(remoteId) {
+    var pc = peerDatabase[remoteId].pc;
+    pc.createOffer(
+      function(sessionDescription) {
+        pc.setLocalDescription(sessionDescription);
+        send('offer', remoteId, sessionDescription);
+      },
+      error
+    );
+  }
+  function handleMessage(message) {
+    var type = message.type,
+      from = message.from,
+      pc = (peerDatabase[from] || addPeer(from)).pc;
+
+    console.log('received ' + type + ' from ' + from);
+
+    switch (type) {
+      case 'init':
+        toggleLocalStream(pc);
+        offer(from);
+        break;
+      case 'offer':
+        pc.setRemoteDescription(new RTCSessionDescription(message.payload), function(){}, error);
+        answer(from);
+        break;
+      case 'answer':
+        pc.setRemoteDescription(new RTCSessionDescription(message.payload), function(){}, error);
+        break;
+      case 'candidate':
+        if(pc.remoteDescription) {
+          pc.addIceCandidate(new RTCIceCandidate({
+            sdpMLineIndex: message.payload.label,
+            sdpMid: message.payload.id,
+            candidate: message.payload.candidate
+          }), function(){}, error);
+        }
+        break;
+    }
+  }
+  function send(type, to, payload) {
+    console.log('sending ' + type + ' to ' + to);
+
+    socket.emit('message', {
+      to: to,
+      type: type,
+      payload: payload
+    });
+  }
+  function toggleLocalStream(pc) {
+    if(localStream) {
+      (!!pc.getLocalStreams().length) ? pc.removeStream(localStream) : pc.addStream(localStream);
+    }
+  }
+  function error(err){
+    console.log(err);
+  }
+
+  return {
+    getId: function() {
+      return localId;
+    },
+
+    setLocalStream: function(stream) {
+
+      // if local cam has been stopped, remove it from all outgoing streams.
+      if(!stream) {
+        for(id in peerDatabase) {
+          pc = peerDatabase[id].pc;
+          if(!!pc.getLocalStreams().length) {
+            pc.removeStream(localStream);
+            offer(id);
+          }
+        }
+      }
+
+      localStream = stream;
+    },
+
+    toggleLocalStream: function(remoteId) {
+      peer = peerDatabase[remoteId] || addPeer(remoteId);
+      toggleLocalStream(peer.pc);
+    },
+
+    peerInit: function(remoteId) {
+      console.info('peerInit')
+      peer = peerDatabase[remoteId] || addPeer(remoteId);
+      send('init', remoteId, null);
+    },
+
+    peerRenegociate: function(remoteId) {
+      offer(remoteId);
+    },
+
+    send: function(type, payload) {
+      socket.emit(type, payload);
+    }
+  };
+
+});
+
+var Peer = function (pcConfig, pcConstraints) {
+  this.pc = new RTCPeerConnection(pcConfig, pcConstraints);
+  this.remoteVideoEl = document.createElement('video');
+  this.remoteVideoEl.controls = true;
+  this.remoteVideoEl.autoplay = true;
+}
+
+
+/*
+ *  Copyright (c) 2014 The WebRTC project authors. All Rights Reserved.
+ *
+ *  Use of this source code is governed by a BSD-style license
+ *  that can be found in the LICENSE file in the root of the source
+ *  tree.
+ */
+
+/* More information about these options at jshint.com/docs/options */
+/* jshint browser: true, camelcase: true, curly: true, devel: true,
+   eqeqeq: true, forin: false, globalstrict: true, node: true,
+   quotmark: single, undef: true, unused: strict */
+/* global mozRTCIceCandidate, mozRTCPeerConnection, Promise,
+mozRTCSessionDescription, webkitRTCPeerConnection, MediaStreamTrack */
+/* exported trace,requestUserMedia */
+'use strict';
+
+var RTCPeerConnection = null;
+var getUserMedia = null;
+var attachMediaStream = null;
+var reattachMediaStream = null;
+var webrtcDetectedBrowser = null;
+var webrtcDetectedVersion = null;
+
+function trace(text) {
+  // This function is used for logging.
+  if (text[text.length - 1] === '\n') {
+    text = text.substring(0, text.length - 1);
+  }
+  if (window.performance) {
+    var now = (window.performance.now() / 1000).toFixed(3);
+    console.log(now + ': ' + text);
+  } else {
+    console.log(text);
+  }
+}
+
+if (navigator.mozGetUserMedia) {
+  console.log('This appears to be Firefox');
+
+  webrtcDetectedBrowser = 'firefox';
+
+  webrtcDetectedVersion =
+    parseInt(navigator.userAgent.match(/Firefox\/([0-9]+)\./)[1], 10);
+
+  // The RTCPeerConnection object.
+  RTCPeerConnection = function(pcConfig, pcConstraints) {
+    // .urls is not supported in FF yet.
+    if (pcConfig && pcConfig.iceServers) {
+      for (var i = 0; i < pcConfig.iceServers.length; i++) {
+        if (pcConfig.iceServers[i].hasOwnProperty('urls')) {
+          pcConfig.iceServers[i].url = pcConfig.iceServers[i].urls;
+          delete pcConfig.iceServers[i].urls;
+        }
+      }
+    }
+    return new mozRTCPeerConnection(pcConfig, pcConstraints);
+  };
+
+  // The RTCSessionDescription object.
+  window.RTCSessionDescription = mozRTCSessionDescription;
+
+  // The RTCIceCandidate object.
+  window.RTCIceCandidate = mozRTCIceCandidate;
+
+  // getUserMedia shim (only difference is the prefix).
+  // Code from Adam Barth.
+  getUserMedia = navigator.mozGetUserMedia.bind(navigator);
+  navigator.getUserMedia = getUserMedia;
+
+  // Shim for MediaStreamTrack.getSources.
+  MediaStreamTrack.getSources = function(successCb) {
+    setTimeout(function() {
+      var infos = [
+        {kind: 'audio', id: 'default', label:'', facing:''},
+        {kind: 'video', id: 'default', label:'', facing:''}
+      ];
+      successCb(infos);
+    }, 0);
+  };
+
+  // Creates ICE server from the URL for FF.
+  window.createIceServer = function(url, username, password) {
+    var iceServer = null;
+    var urlParts = url.split(':');
+    if (urlParts[0].indexOf('stun') === 0) {
+      // Create ICE server with STUN URL.
+      iceServer = {
+        'url': url
+      };
+    } else if (urlParts[0].indexOf('turn') === 0) {
+      if (webrtcDetectedVersion < 27) {
+        // Create iceServer with turn url.
+        // Ignore the transport parameter from TURN url for FF version <=27.
+        var turnUrlParts = url.split('?');
+        // Return null for createIceServer if transport=tcp.
+        if (turnUrlParts.length === 1 ||
+          turnUrlParts[1].indexOf('transport=udp') === 0) {
+          iceServer = {
+            'url': turnUrlParts[0],
+            'credential': password,
+            'username': username
+          };
+        }
+      } else {
+        // FF 27 and above supports transport parameters in TURN url,
+        // So passing in the full url to create iceServer.
+        iceServer = {
+          'url': url,
+          'credential': password,
+          'username': username
+        };
+      }
+    }
+    return iceServer;
+  };
+
+  window.createIceServers = function(urls, username, password) {
+    var iceServers = [];
+    // Use .url for FireFox.
+    for (var i = 0; i < urls.length; i++) {
+      var iceServer =
+        window.createIceServer(urls[i], username, password);
+      if (iceServer !== null) {
+        iceServers.push(iceServer);
+      }
+    }
+    return iceServers;
+  };
+
+  // Attach a media stream to an element.
+  attachMediaStream = function(element, stream) {
+    console.log('Attaching media stream');
+    element.mozSrcObject = stream;
+  };
+
+  reattachMediaStream = function(to, from) {
+    console.log('Reattaching media stream');
+    to.mozSrcObject = from.mozSrcObject;
+  };
+
+} else if (navigator.webkitGetUserMedia) {
+  console.log('This appears to be Chrome');
+
+  webrtcDetectedBrowser = 'chrome';
+  // Temporary fix until crbug/374263 is fixed.
+  // Setting Chrome version to 999, if version is unavailable.
+  var result = navigator.userAgent.match(/Chrom(e|ium)\/([0-9]+)\./);
+  if (result !== null) {
+    webrtcDetectedVersion = parseInt(result[2], 10);
+  } else {
+    webrtcDetectedVersion = 999;
+  }
+
+  // Creates iceServer from the url for Chrome M33 and earlier.
+  window.createIceServer = function(url, username, password) {
+    var iceServer = null;
+    var urlParts = url.split(':');
+    if (urlParts[0].indexOf('stun') === 0) {
+      // Create iceServer with stun url.
+      iceServer = {
+        'url': url
+      };
+    } else if (urlParts[0].indexOf('turn') === 0) {
+      // Chrome M28 & above uses below TURN format.
+      iceServer = {
+        'url': url,
+        'credential': password,
+        'username': username
+      };
+    }
+    return iceServer;
+  };
+
+  // Creates an ICEServer object from multiple URLs.
+  window.createIceServers = function(urls, username, password) {
+    return {
+      'urls': urls,
+      'credential': password,
+      'username': username
+    };
+  };
+
+  // The RTCPeerConnection object.
+  RTCPeerConnection = function(pcConfig, pcConstraints) {
+    return new webkitRTCPeerConnection(pcConfig, pcConstraints);
+  };
+
+  // Get UserMedia (only difference is the prefix).
+  // Code from Adam Barth.
+  getUserMedia = navigator.webkitGetUserMedia.bind(navigator);
+  navigator.getUserMedia = getUserMedia;
+
+  // Attach a media stream to an element.
+  attachMediaStream = function(element, stream) {
+    if (typeof element.srcObject !== 'undefined') {
+      element.srcObject = stream;
+    } else if (typeof element.mozSrcObject !== 'undefined') {
+      element.mozSrcObject = stream;
+    } else if (typeof element.src !== 'undefined') {
+      element.src = URL.createObjectURL(stream);
+    } else {
+      console.log('Error attaching stream to element.');
+    }
+  };
+
+  reattachMediaStream = function(to, from) {
+    to.src = from.src;
+  };
+} else {
+  console.log('Browser does not appear to be WebRTC-capable');
+}
+
+// Returns the result of getUserMedia as a Promise.
+function requestUserMedia(constraints) {
+  return new Promise(function(resolve, reject) {
+    var onSuccess = function(stream) {
+      resolve(stream);
+    };
+    var onError = function(error) {
+      reject(error);
+    };
+
+    try {
+      getUserMedia(constraints, onSuccess, onError);
+    } catch (e) {
+      reject(e);
+    }
+  });
+}
+
+if (typeof module !== 'undefined') {
+  module.exports = {
+    PeerManager: PeerManager,
+    Peer: Peer,
+    RTCPeerConnection: RTCPeerConnection,
+    getUserMedia: getUserMedia,
+    attachMediaStream: attachMediaStream,
+    reattachMediaStream: reattachMediaStream,
+    webrtcDetectedBrowser: webrtcDetectedBrowser,
+    webrtcDetectedVersion: webrtcDetectedVersion,
+    requestUserMedia: requestUserMedia,
+    //requestUserMedia: not exposed on purpose.
+    //trace: not exposed on purpose.
+  };
+}
